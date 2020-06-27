19e8fc113b7f0e2556dfde2a6bbe9dcc8eed4994
seema1611 <seemarajpure16@gmail.com>
2020-06-27 17:03:19 +0530

    ADDED: Used user model class for passing data

---
+++ src/main/java/com/loginregistration/model/User.java
@@ -0,0 +1,82 @@
+package com.loginregistration.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    public int id;
+
+    public String userName;
+
+    public String password;
+
+    public String emailId;
+
+    public String address;
+
+    public LocalDateTime registerDate;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String emailId, String address) {
+        this.userName = userName;
+        this.password = password;
+        this.emailId = emailId;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+}
